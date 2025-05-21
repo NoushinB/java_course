1f9115a14b042412951526ac8c35ac0c96d72691
noushinB <nou.babakhani@gmail.com>
2025-05-21 18:18:28 +0100

    add methods

--- src/main/java/org/noushin/examples/myapp/Car.java
+++ src/main/java/org/noushin/examples/myapp/Car.java
@@ -1,5 +1,13 @@
 package org.noushin.examples.myapp;
 
 public class Car {
-
+    public void drive(){
+        System.out.println("The cars is driving.");
+    }
+    public  void  stop(){
+        System.out.println("The car has stopped.");
+    }
+    public  void  honk(){
+        System.out.println("The car is honking.");
+    }
 }
