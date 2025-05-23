19bfda6c0f77e87a570d8e6317c15d37625ae462
noushinB <nou.babakhani@gmail.com>
2025-05-23 18:46:53 +0100

    create object

--- src/main/java/org/noushin/examples/Main.java
+++ src/main/java/org/noushin/examples/Main.java
@@ -1,5 +1,8 @@
 package org.noushin.examples;
 
+
+import org.noushin.examples.myapp.Car;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +12,10 @@ public class Main {
         double myDoubleNum=5.25;
         char myLetter='N';
         boolean myBool=true;
+
+        Car myCar=new Car();
+        myCar.drive();
+        myCar.stop();
 
     }
 }
