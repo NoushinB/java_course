d94568bc189d111805c2f677746ba557d3dfaa8a
noushinB <nou.babakhani@gmail.com>
2025-06-20 10:55:05 +0100

    for loop added

--- src/main/java/org/noushin/examples/Main.java
+++ src/main/java/org/noushin/examples/Main.java
@@ -2,15 +2,20 @@ package org.noushin.examples;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int x=18;
-        int y=18;
-
-        if (x>y){
-            System.out.println("x is greater than y");
-        } else if (x<y) {
-            System.out.println("x is smaller than y");
-        } else {
-            System.out.println("x is equal to y");
+        //Print the numbers 0 through 4.
+        for (int i=0;i<5;i++){
+            System.out.println(i);
         }
+
+        //Print numbers in a loop and after each number print three *
+        for (int i=1;i<=2;i++){
+            System.out.println(i);
+            for (int j=1;j<=3;j++){
+                System.out.println("*");
+
+            }
+        }
+
+
     }
 }
