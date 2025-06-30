c8a667cbcbb880fefc110d4f3de4ddc716191008
noushinB <nou.babakhani@gmail.com>
2025-06-30 12:14:08 +0100

    Array added

--- src/main/java/org/noushin/examples/Main.java
+++ src/main/java/org/noushin/examples/Main.java
@@ -1,20 +1,18 @@
 package org.noushin.examples;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Print the numbers 0 through 4.
-        for (int i=0;i<5;i++){
-            System.out.println(i);
-        }
+        //String[] cars;
+       // cars=new String[]{"Volvo","BmW","Ford","Mazda"};
 
-        //Print numbers in a loop and after each number print three *
-        for (int i=1;i<=2;i++){
-            System.out.println(i);
-            for (int j=1;j<=3;j++){
-                System.out.println("*");
+        String[] cars={"Volvo","BmW","Ford","Mazda"};
+       //Print total numbers of cars
+        System.out.println("Totalcars:"+ cars.length);
 
-            }
-        }
+        int[] num={2,6,5,67};
+
+
+
 
 
     }
