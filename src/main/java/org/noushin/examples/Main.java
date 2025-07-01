7cf1d7f7f38a0e20e706d692ccd7a432714c5618
noushinB <nou.babakhani@gmail.com>
2025-07-01 16:40:38 +0100

    switch added

--- src/main/java/org/noushin/examples/Main.java
+++ src/main/java/org/noushin/examples/Main.java
@@ -2,18 +2,20 @@ package org.noushin.examples;
 
 public class Main {
     public static void main(String[] args) {
-        //String[] cars;
-       // cars=new String[]{"Volvo","BmW","Ford","Mazda"};
+        // This example is for weekdays; specifically, it maps numbers (1 to 7) to days of the week using a switch statement..
+        int day=4;
+       String dayName= switch (day){
+           case 1 -> "Monday";
+           case 2 -> "Tuesday";
+           case 3 -> "Wednesday";
+           case 4 -> "Thursday";
+           case 5 -> "Friday";
+           case 6 -> "Saturday";
+           case 7 -> "Sunday";
+           default -> "Invalid day";
 
-        String[] cars={"Volvo","BmW","Ford","Mazda"};
-       //Print total numbers of cars
-        System.out.println("Totalcars:"+ cars.length);
-
-        int[] num={2,6,5,67};
-
-
-
-
+        };
+       System.out.println(dayName);
 
     }
 }
