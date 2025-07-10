92714ce532a753de9ae53f15fa90e64525f7c583
noushinB <nou.babakhani@gmail.com>
2025-07-10 15:45:37 +0100

    episod15 image added

--- src/main/java/org/noushin/examples/Main.java
+++ src/main/java/org/noushin/examples/Main.java
@@ -2,20 +2,13 @@ package org.noushin.examples;
 
 public class Main {
     public static void main(String[] args) {
-        // This example is for weekdays; specifically, it maps numbers (1 to 7) to days of the week using a switch statement..
-        int day=4;
-       String dayName= switch (day){
-           case 1 -> "Monday";
-           case 2 -> "Tuesday";
-           case 3 -> "Wednesday";
-           case 4 -> "Thursday";
-           case 5 -> "Friday";
-           case 6 -> "Saturday";
-           case 7 -> "Sunday";
-           default -> "Invalid day";
-
-        };
-       System.out.println(dayName);
+        // Countdown timer from 3 that ends with celebration message.
+        int countdown=3;
+        while (countdown>0){
+            System.out.println(countdown);
+            countdown--;
+        }
+       System.out.println("Happy New Year!!");
 
     }
 }
